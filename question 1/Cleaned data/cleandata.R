c6a700c56fe7db54881d0754faaa633ae55da329
Roshan003 <60837794+Roshan003@users.noreply.github.com>
2024-02-12 22:04:26 -0600

    Add files via upload

---
+++ question 1/Cleaned data/cleandata.R
@@ -0,0 +1,8 @@
+raw_yield_data <- read.csv("~/Downloads/q1PDS.csv")
+View (raw_yield_data)
+#attach(raw_yield_data)
+cleaned_yield_data <- na.omit (raw_yield_data[raw_yield_data$Frailty !="NA",])
+View (cleaned_yield_data)
+
+plot(raw_yield_data$Height..inches,raw_yield_data$Weight..lbs,xlab="Height",ylab="Weight",main=("plot between height and weight"))
+
