2de552a9698ef743b4e6419b121a3c3bb5a9d7a1
Roshan003 <60837794+Roshan003@users.noreply.github.com>
2024-02-12 23:59:08 -0600

    Update cleandata.R

--- Question1/Cleaned data/cleandata.R
+++ Question1/Cleaned data/cleandata.R
@@ -4,5 +4,4 @@ View (raw_yield_data)
 cleaned_yield_data <- na.omit (raw_yield_data[raw_yield_data$Frailty !="NA",])
 View (cleaned_yield_data)
 
-plot(raw_yield_data$Height..inches,raw_yield_data$Weight..lbs,xlab="Height",ylab="Weight",main=("plot between height and weight"))
-
+plot(raw_yield_data$Height,raw_yield_data$Weight,xlab="Height",ylab="Weight",main=("plot between height and weight"))
